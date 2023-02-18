d41580e964c1c29a02b04e403a07e2ba5cb56bd5
Дмитрий <kuzmikd@mail.ru>
2023-02-18 15:27:03 +0300

    feat:add human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function toReadable (number) {
-  
+    const num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    const tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    if (number < 20) return num[number];
+    const digit = number % 10;
+    if (number < 100) return tens[~~ (number / 10) - 2] + (digit? " " + num[digit] : "");
+    if (number < 1000) return num[~~ (number / 100)] + " hundred" + (number % 100 == 0? "" : " " + toReadable(number % 100));
 }
